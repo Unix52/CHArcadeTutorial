b7ed4eb230e93c8f49f39c24660b0a0e47adbbad
αdяiαиcf <Unix52@users.noreply.github.com>
2016-02-14 18:52:38 -0200

    Create Tutorial.java

---
+++ Tutorial.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+import java.util.ArrayList;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import me.acf.MiniGames.MiniGamesMananger;
+import me.acf.MiniGames.MiniGamesMananger.Time;
+import me.hub.MiniPlugin;
+
+
+
+/**
+ * @author adriancf
+ *
+ */
+public class Arcade  {
+
+	/**
+	 * @param plugin this da onEnable
+	 */
+	
+	public void Arcade(JavaPlugin plugin) {
+	       //120 tempo para iniciar o game
+	       MiniGamesMananger mine = new MiniGamesMananger(plugin,120, _moduleName);
+         mine.CriarTime(Time.Vermelho); // Criar os Times
+         mine.CriarTime(Time.Amarelo);
+         mine.CriarTime(Time.Verder);
+         mine.CriarTime(Time.Azul);
+         mine.teamselection = Bukkit.getWorld("world").getSpawnLocation();  // setar os locais de escolher os times
+         
+	}
+}
